a6f4c91a1382c8c01af9f9258d3ef0f29d8a11db
Luke Matthew Sutton <me@lukematthewsutton.com>
2016-03-22 10:12:08 +1030

    Namespace the tag and attribute structs

--- Sources/Attributes.swift
+++ Sources/Attributes.swift
@@ -1,31 +1,43 @@
-public struct Src: Attribute, SimpleAttribute {
-  public let label = "attr"
-  public var value: String
-  public init(_ value: String) {
-    self.value = value
+public struct Attr {
+  private init() {}
+  
+  public struct Src: Attribute, SimpleAttribute {
+    public let label = "attr"
+    public var value: String
+    public init(_ value: String) {
+      self.value = value
+    }
   }
-}
 
-public struct ClassName: Attribute, SimpleAttribute {
-  public let label = "class"
-  public var value: String
-  public init(_ value: String) {
-    self.value = value
+  public struct ClassName: Attribute, SimpleAttribute {
+    public let label = "class"
+    public var value: String
+    public init(_ value: String) {
+      self.value = value
+    }
   }
-}
 
-public struct ID: Attribute, SimpleAttribute {
-  public let label = "id"
-  public var value: String
-  public init(_ value: String) {
-    self.value = value
+  public struct ID: Attribute, SimpleAttribute {
+    public let label = "id"
+    public var value: String
+    public init(_ value: String) {
+      self.value = value
+    }
   }
-}
 
-public struct Href: Attribute, SimpleAttribute {
-  public let label = "href"
-  public var value: String
-  public init(_ value: String) {
-    self.value = value
+  public struct Href: Attribute, SimpleAttribute {
+    public let label = "href"
+    public var value: String
+    public init(_ value: String) {
+      self.value = value
+    }
+  }
+
+  public struct Title: Attribute, SimpleAttribute {
+    public let label = "title"
+    public var value: String
+    public init(_ value: String) {
+      self.value = value
+    }
   }
 }
--- Tests/Hekk/HekkTests.swift
+++ Tests/Hekk/HekkTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class HekkTests: XCTestCase {
   func testDivWithAttributes() {
-    let tag = Div([ID("header")], P("no"))
+    let tag = Tag.Div([Attr.ID("header")], Tag.P("no"))
     XCTAssertEqual(tag.stringValue, "<div id=\"header\"><p>no</p></div>")
   }
 }
