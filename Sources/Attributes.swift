bb809141910dc80f7da8cf7bc7706590941bbc5b
Luke Matthew Sutton <me@lukematthewsutton.com>
2016-04-08 21:04:28 +0930

    Begin fleshing out attributes

--- Sources/Attributes.swift
+++ Sources/Attributes.swift
@@ -1,6 +1,64 @@
 public struct Attributes {
   private init() {}
 
+  // Form, input
+  public struct Accept: Attribute {
+    public let label = "accept"
+    public var stringValue: String
+    public init(_ value: String) {
+      self.stringValue = value
+    }
+  }
+
+  public struct Name: Attribute {
+    public let label = "name"
+    public var stringValue: String
+    public init(_ value: String) {
+      self.stringValue = value
+    }
+  }
+
+  // Form
+  public struct AcceptCharset: Attribute {
+    public let label = "accept-charset"
+    public var stringValue: String
+    public init(_ value: String) {
+      self.stringValue = value
+    }
+  }
+
+  // Form
+  public struct Method: Attribute {
+    public enum MethodValue {
+      case GET
+      case PUT
+    }
+
+    public let label = "method"
+    public var stringValue: String
+    public init(_ value: MethodValue) {
+      self.stringValue = String(value)
+    }
+  }
+
+  // Global
+  public struct AccessKey: Attribute {
+    public let label = "accesskey"
+    public var stringValue: String
+    public init(_ value: String) {
+      self.stringValue = value
+    }
+  }
+
+  // Form
+  public struct Action: Attribute {
+    public let label = "action"
+    public var stringValue: String
+    public init(_ value: String) {
+      self.stringValue = value
+    }
+  }
+
   public struct Src: Attribute {
     public let label = "src"
     public var stringValue: String
